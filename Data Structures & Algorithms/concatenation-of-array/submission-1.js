0a2da78064bdc49d8b952ba4499099c5f7bdf347
ratnadeep konar <ratnadeep.konar@gmail.com>
2026-04-06 21:31:40 +0530

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+        let n = nums.length;
+        const ans = new Array(n * 2);
+        for(let i = 0; i < n; i++) {
+            ans[i] = ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+}
