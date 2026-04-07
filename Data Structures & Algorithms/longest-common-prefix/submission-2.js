a3c97d316115cff2dcff4e091019cff3d9cd3e29
ratnadeep konar <ratnadeep.konar@gmail.com>
2026-04-07 08:07:08 +0530

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    //TC: O(n * mlogm)
+    longestCommonPrefix(strs) {
+        if (strs.length === 1) {
+            return strs[0];
+        }
+
+        strs.sort();
+        let n = Math.min(strs[0].length, strs[strs.length - 1].length);
+        for (let i = 0; i < n; i++) {
+            if (strs[0][i] !== strs[strs.length - 1][i]) {
+                return strs[0].slice(0, i);
+            }
+        }
+        return strs[0];
+    }
+}
