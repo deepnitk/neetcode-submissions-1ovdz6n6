d12aa98f7ddf1bddb09eceeeff4ab4f07c0e0ae5
ratnadeep konar <ratnadeep.konar@gmail.com>
2026-06-15 21:17:50 +0530

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let res = "";
+        for (const s of strs) {
+            res += s.length + '#' + s;
+        }
+        return res;
+    }
+    
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        //5#hello5#world
+        const res = [];
+        let ptr = 0;
+        while (ptr < str.length) {
+            let j = ptr;
+            while (str[j] !== '#') {
+                j++;
+            }
+            const n = parseInt(str.substring(ptr, j));
+            ptr = j + 1;
+            let word = str.slice(ptr, ptr + n);
+            res.push(word);
+            ptr = ptr + n;
+        }
+        return res;
+    }
+}
