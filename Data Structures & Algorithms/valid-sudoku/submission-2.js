2391747e3d7a781a6420bccf598184000ff34662
ratnadeep konar <ratnadeep.konar@gmail.com>
2026-06-17 20:07:46 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.js
@@ -0,0 +1,50 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        const m = board.length;
+        const n = board[0].length;
+
+        for (let r = 0; r < m; r++) {
+            const hs = new Set();
+            for (let c = 0; c < n; c++) {
+                if (board[r][c] !== '.') {
+                    if (hs.has(board[r][c])) 
+                        return false;
+                    hs.add(board[r][c]);
+                }
+            }
+        }
+
+        for (let c = 0; c < n; c++) {
+            const hs = new Set();
+            for (let r = 0; r < n; r++) {
+                if (board[r][c] !== '.') {
+                    if (hs.has(board[r][c])) 
+                        return false;
+                    hs.add(board[r][c]);
+                }
+            }
+        }
+
+        for (let boxRow = 0; boxRow < 3; boxRow++) {
+            for (let boxCol = 0; boxCol < 3; boxCol++) {
+                const hs = new Set();
+                
+                for (let r = boxRow * 3; r < boxRow * 3 + 3; r++) {
+                    for (let c = boxCol * 3; c < boxCol * 3 + 3; c++) {
+                        if (board[r][c] !== '.') {
+                            if (hs.has(board[r][c])) {
+                                return false;
+                            }
+                            hs.add(board[r][c]);
+                        }
+                    }
+                }
+            }
+        }
+        return true
+    }
+}
